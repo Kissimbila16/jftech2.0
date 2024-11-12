571f48e0cf063ae5b66f65ec847a7ac8a9f96fc6
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-12 16:05:13 +0000

    fix: erro resolvido da classe no icon

--- src/app/components/cardmax/index.js
+++ src/app/components/cardmax/index.js
@@ -2,8 +2,8 @@ export default function Cardmax({ key, title, text, link, num }) {
   return (
     <>
       <div className="card ">
-        <div className="card-body bg-danger bg d-flex justify-content-around w-100">
-          <h4 className="card-title">{title} </h4>
+        <div className="card-body bg-danger card-w d-flex justify-content-around w-100">
+di
           <p className="card-text"> {text} </p>
          <div className="d-grid">
          <a href="#" className="card-link">
--- src/app/layouts/Nav/index.js
+++ src/app/layouts/Nav/index.js
@@ -17,7 +17,7 @@ export default function Nav() {
           <Nav_text name="Contacto" way="#home" />
         </ul>
         <Btn_Contato_Nos name={'Orcamento'} />
-        <i class="bi bi-list fs-3"></i>
+        <i className="bi bi-list fs-3"></i>
         &nbsp;
       </div>
     </nav>
