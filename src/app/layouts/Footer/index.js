ff96fdcef2c839fa5afab408ef837377ad0e04da
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-21 14:16:22 +0000

    instalamdo lib para add sidebar

--- src/app/layouts/Footer/index.js
+++ src/app/layouts/Footer/index.js
@@ -7,13 +7,13 @@ export default function Footer() {
     <>
       <footer className="mt-5">
         <section className="w-100 justify-content-around border-simple layout-auto" id="flex-grid">
-          <div className="d-grid w-100 my-5 ">
+          <div className="d-grid my-5 ">
             <h4 className="py-3 fs-3">
               <Icon />
             </h4>
-            <section className="tam-100 d-flex mx-5">
+            <section id="listFooter1" className="tam-100 d-flex mx-5">
               <ol className="list-footer txt-left mx-5 w-100">
-                <li className="top-footer">
+                <li className="top-footer w-full my-0">
                   <h5 className="fs-6 w-100">Política de Privacidade</h5>
                   <h5 className="fs-6 w-100">Termos de Uso</h5>
                 </li>
@@ -21,12 +21,12 @@ export default function Footer() {
             </section>
           </div>
           <div className="d-grid w-100 my-5 ">
-            <h4 className="py-3 fs-4">
+            <h4 className="py-3 fs-4" id="footer-text">
               
               Institucional
             </h4>
-            <section className="tam-100 d-flex mx-5">
-              <ol className="list-footer mx-5 txt-left">
+            <section id="listFooter2" className="d-flex mx-5">
+              <ol className="list-footer mx-5 txt-left w-full">
                 <Link href="#home" className="mx-5">Home </Link>
                 <Link href="#home" className="mx-5">Jftech </Link>
                 <Link href="#home" className="mx-5"> Solucoes</Link>
@@ -37,12 +37,12 @@ export default function Footer() {
               </ol>
             </section>
           </div>
-          <div className="d-grid w-100 my-5 ">
-            <h4 className="py-3 fs-4">
+          <div className="d-grid  my-5">
+            <h4 className="py-3 fs-4" id="footer-text">
                Horários
             </h4>
-            <section className="tam-100 d-flex mx-5">
-              <ol className="list-footer mx-5 top-footer" >
+            <section id="listFooter3" className="tam-100 d-flex mx-5">
+              <ol className="list-footer mx-5 top-footer " >
                 <li className="mt-5">
                   <h5 className="mt-3">teste</h5>
                 </li>
@@ -52,12 +52,12 @@ export default function Footer() {
               </ol>
             </section>
           </div>
-          <div className="d-grid center wid my-5 ">
+          <div className="d-grid center w-xxl  my-5 ">
             <h4 className="py-3 fs-4 px-4">
                Siga-nos
             </h4>
-            <section className="tam-100 d-flex mx-5">
-              <ol className="list-footer mx-5" style={{marginTop:'-30%'}}>
+            <section id="listFooter4" className="tam-100 d-flex mx-5 w100">
+              <ol className="list-footer mx-5 px-1" style={{marginTop:'-30%'}}>
                 <li className="mx-5 d-flex">
                 <Link href={'#'}><i className="bi bi-instagram fs-4 mx-2"></i></Link>
                 <Link href={'#'}><i className="bi bi-linkedin fs-4 mx-2"></i></Link>
